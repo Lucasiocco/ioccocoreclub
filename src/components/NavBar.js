468ca56d6999fe1560c3534a7cce945b41ad4425
Lucas Nahuel Iocco <lucas.nahuel.iocco@gmail.com>
2022-05-30 10:36:57 -0300

    Se agrego bootstrap al navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,21 @@
 const NavBar = () => {
     return (
-        <section class="navigation">
-  <div class="nav-container">
-    <div class="brand">
-      <a href="#!">CoreClub</a>
-    </div>
-    <nav>
-      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
-      <ul class="nav-list">
-        <li>
-          <a href="#!">Home</a>
-        </li>
-        <li>
-          <a href="#!">About</a>
-        </li>
-        <li>
-          <a href="#!">Services</a>
-          <ul class="nav-dropdown">
-            <li>
-              <a href="#!">Entrenamientos Personalizados</a>
-            </li>
-            <li>
-              <a href="#!">Seguimiento Online</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#!">Contact</a>
-        </li>
-      </ul>
-    </nav>
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">TheCoreClub</a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nosotros</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tienda</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+    </ul>
   </div>
-</section>
+      </nav>
     )
 };
 
