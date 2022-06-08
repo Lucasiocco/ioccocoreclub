eb9e73d9317622b20b2474ea4e3ff2fa94685d63
Lucas Nahuel Iocco <lucas.nahuel.iocco@gmail.com>
2022-06-08 11:05:57 -0300

    Se agrego useEffect y setTimeout

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
 import ItemCount from "./ItemCount";
 
 const ItemListContainer = (props) => {
-    return (
-        <>
-        <h1>
-            {props.greeting}	
-        </h1>
-        <ItemCount />
-        </>
-    )
+
+    const [items, setItems] = useState([]);
+
+  useEffect(() => {
+      setTimeout(() => {
+          console.log("Cargando items");
+          const productos = ["Producto 1", "Producto 2", "Producto 3"];
+          setItems (productos);
+      },3000)
+  })
+   
+
+    const onAdd = () => {}
+
+    if (items.length > 0) {
+        return (
+            <div>
+                <h1>{props.greeting}</h1>
+                <ItemCount stock={items} initial={0} onAdd={onAdd} />
+            </div>
+        )
+    } else { 
+        return (
+            <div>
+                <h1>{props.greeting}</h1>
+                <p>Cargando productos...</p>
+            </div>
+        )
+    }
 }
 
 export default ItemListContainer
